2a47ad1505f2e36e252a56b6eca010dab525571e
satoru <satoru@wkbr.net>
2015-07-09 14:17:53 +0900

    ニュースサイトの最初のコミット

---
+++ www/cron.php
@@ -0,0 +1,156 @@
+<?php
+include_once(dirname(__FILE__).'/lib/magpierss-0.72/rss_fetch.inc');
+include_once(dirname(__FILE__).'/lib/magpierss-0.72/rss_utils.inc');
+//include_once(dirname(__FILE__).'/lib/Curl.class.inc');
+$source = parse_ini_file(dirname(__FILE__).'/source.ini',true);
+$fbapi    = 'http://graph.facebook.com/?id=';
+$twapi    = 'http://urls.api.twitter.com/1/urls/count.json?url=';
+$blogs = array();
+$unique   = array();
+// データの取得
+foreach($source as $v){
+    $url = $v['url'];
+    $rss = fetch_rss($url);
+    if(!$rss){
+        var_dump($v);
+        continue;
+    }
+    foreach($rss->items as $content){
+        $cc = new content($content);
+        // 24時間以内のデータのみ
+        if($cc->getDate() < time() - 24*60*60){
+            continue;
+        }
+        // urlの重複を避ける
+        if(!$cc->getLink() || in_array($cc->getLink(),$unique)){
+            continue;
+        }
+        $unique[]   = $cc->getLink();
+        $tmp = $cc->getAll();
+        if(isset($v['tag'])){
+            $tmp['tag'] = $v['tag'];
+        }
+        $blogs[] = $tmp;
+    }
+}
+// シェア数の一括取得
+/*
+array_walk($unique,function(&$v,$k,$fbapi){ $v = $fbapi.$v; },$fbapi);
+$curl = new curl;
+$curl->setUrl($unique);
+$curl->exec();
+$res = $curl->getRes();
+*/
+$res = array();
+foreach($unique as $i => $v){
+	$res[$i] = array();
+	$res[$i]['fb'] = file_get_contents($fbapi.$v);
+	$res[$i]['tw'] = file_get_contents($twapi.$v);
+}
+$tmp = array();
+foreach($blogs as $i => $v){
+	$fbc = json_decode($res[$i]['fb']);
+	$twc = json_decode($res[$i]['tw']);
+	$tmp[$i] = $v;
+	$tmp[$i]['fb'] = isset($fbc->shares)?(int)$fbc->shares:0;
+	$tmp[$i]['tw'] = isset($twc->count)?(int)$twc->count:0;
+	$tmp[$i]['ttl'] = $tmp[$i]['fb'] + $tmp[$i]['tw'];
+}
+$blogs = $tmp;
+// total順にソート
+usort($blogs,function($a,$b){
+    return $a['ttl'] < $b['ttl'];
+});
+if($blogs[0]['ttl'] == 0){
+    var_dump('total 0 error');
+    exit();
+}
+
+// データファイルに保存
+file_put_contents(dirname(__FILE__).'/blogs.txt',serialize($blogs));
+
+// {{{ class content
+class content{
+    public $content = array();
+    public function __construct($content){
+        $this->content = $content;
+    }
+    public function getAll(){
+        $tmp = array(
+                'title' => htmlspecialchars($this->get('title')),
+                'link'  => strip_tags($this->getLink()),
+                'date'  => $this->getDate(),
+                'img'   => strip_tags($this->getImgUrl()),
+                );
+        return $tmp;
+    }
+    public function getImgUrl(){
+        $tmp = array('description','content/encoded');
+        foreach($tmp as $v){
+            $pattern = '@<img.+?src="(http://.+?)"(.*?)>@';
+            preg_match_all($pattern, xpath($this->content,$v), $m);
+            foreach($m[1] as $w){
+                if(isset($w)&&!inStr($w,array('hatena','rss'))){
+                    return $w;
+                }
+            }
+        }
+        return sprintf('http://s.wordpress.com/mshots/v1/%s?w=320',urlencode($this->getLink()));
+    }
+    public function getDate(){
+        $tmp = array('date','pubdate','dc/date','issued');
+        foreach($tmp as $v){
+            $c = xpath($this->content,$v);
+            if($c){
+                return strtotime($c);
+            }
+        }
+    }
+    public function getLink(){
+        $tmp = array('link','guid');
+        $through = array('headlines.yahoo.co.jp',
+                'youtube.com',
+                'groups.google.com/forum',
+                'facebook.com'
+                );
+        foreach($tmp as $v){
+            $c = xpath($this->content,$v);
+            if(strrpos($c,'?')&&!inStr($c,$through)){
+                $c = substr($c,0,strrpos($c,'?'));
+            }
+            if($c && !strpos($c,'rss')){
+                return $c;
+            }
+        }
+    }
+    public function get($param){
+        //return isset($this->content[$param])?$this->content[$param]:'';
+        return $this->content[$param];
+    }
+}
+// }}}
+
+// {{{ function
+// 配列にpathでアクセス
+function xpath($array,$path){
+    if(!is_array($array)){ return false; }
+    foreach(explode('/',$path) as $v){
+        if(!isset($array[$v])){
+            return false;
+        }
+        $array = $array[$v];
+    }
+    return $array; 
+}
+// 配列から文字列の検索
+function inStr($str,$needle){
+    foreach((array)$needle as $v){
+        if(strpos($str,$v)!==false){
+            return true;
+        }
+    }
+    return false;
+}
+// }}}
+
+?>
---
+++ www/index.php
@@ -0,0 +1,117 @@
+<?php
+$tmp = file_get_contents(dirname(__FILE__).'/blogs.txt');
+$blogs = unserialize($tmp);
+$tmp = array();
+$tagl = array('life','tech','game','neta');
+// タグでの絞込
+$tag = isset($_GET['t'])?$_GET['t']:false;
+if($tag){
+    foreach($blogs as $v){
+        if(in_array($tag,$v['tag'])){
+            $tmp[] = $v;
+        }
+    }
+    $blogs = $tmp;
+}
+?>
+<html>
+<head>
+<title>わくぶるにゅーす</title>
+<meta name="viewport" content="width=device-width"/>
+<meta name="date" content="<?php echo date(DATE_ISO8601,strtotime(date('Y-m-d H:00:00'))); ?>" />
+<meta name="description" content="わくぶるするニュースを集めています。"/>
+<meta name="keywords" content="facebook,ランキング,ニュース"/>
+<meta property="og:title" content="わくぶるにゅーす" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://antenna.lant.jp" />
+<meta property="og:image" content="" />
+<meta property="og:site_name" content="わくぶるにゅーす" />
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="">
+<meta name="twitter:title" content="わくぶるにゅーす">
+<meta name="twitter:description" content="わくぶるするニュースを集めています。">
+<meta name="twitter:creator" content="@wkwkan">
+<meta name="twitter:image:src" content="">
+<meta name="twitter:domain" content="news.wkbr.net/">
+<link rel="stylesheet" href="/static/css/default.css" type="text/css" />
+<link rel="canonical" href="http://news.wkbr.net/">
+</head>
+<body>
+<div id="wrap">
+
+<div id="head">
+<h1><a href="/">わくぶるにゅーす</a></h1>
+<div id="nav">
+<ul>
+<li><a href="#" class="navImage">画像ビュー</a></li>
+<li><a href="#" class="navList">リストビュー</a></li>
+</ul>
+</div>
+</div>
+
+<div id="taglist">
+<ul>
+<li><a href="/">Top</a></li>
+<?php foreach($tagl as $v): ?>
+<li><a href="/t/<?php echo $v; ?>"><?php echo ucfirst($v); ?></a></li>
+<?php endforeach; ?>
+</ul>
+</div>
+
+<div id="container" class="typeImage">
+<?php foreach($blogs as $i => $v): ?>
+<div class="out">
+<div class="box" style="background-image: url('<?php echo $v['img'];?>');">
+<div class="top">
+<span class="fb"><?php echo $v['fb'];?></span>
+<span class="tw"><?php echo $v['tw'];?></span>
+</div>
+<div class="bottom">
+<a href="<?php echo $v['link'];?>" target="_brank">
+<p><?php echo $v['title'];?></p>
+<time class="date"><?php echo date('Y/m/d G:i',$v['date']);?></time>
+</a>
+</div>
+</div>
+</div>
+<?php
+endforeach;
+?>
+</div>
+</div>
+
+
+<div id="foot">
+<p>情報提供,相互リンクお待ちしております。</p>
+<iframe src="https://docs.google.com/forms/d/13U4l6cxZp5p4cR5awuTyadQ957CAgwBJubM3rTwCHyU/viewform?embedded=true">読み込み中...</iframe>
+</div>
+</div>
+<script src="/static/js/jquery-2.1.0.min.js"></script>
+<script>
+(function($){
+	var $container = $("#container"),
+		$navImage = $(".navImage"),
+		$navList  = $(".navList");
+	$navImage.on("click", function(e){
+		e.preventDefault();
+		$container.removeClass("typeList").addClass("typeImage");
+	});
+	$navList.on("click", function(e){
+		e.preventDefault();
+		$container.removeClass("typeImage").addClass("typeList");
+	});
+
+})(jQuery);
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-56794611-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
